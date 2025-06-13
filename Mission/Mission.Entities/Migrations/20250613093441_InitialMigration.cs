a1c56d25457dfdea54332866299d540952c34c70
vidhi123-alt456 <vidhithakkar1837@gmail.com>
2025-06-13 16:42:02 +0530

    add files

---
+++ Mission/Mission.Entities/Migrations/20250613093441_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "first_name", "last_name", "password" },
+                values: new object[] { "vidhi@gmail.com", "Vidhi", "Thakkar", "vidhi@123" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "first_name", "last_name", "password" },
+                values: new object[] { "admin@tatvasoft.com", "Tatva", "Admin", "admin" });
+        }
+    }
+}
---
+++ Mission/Mission.Services/IServices/ILoginService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission.Entities;
+using Mission.Entities.Models;
+namespace Mission.Services.IServices
+{
+    public interface ILoginService
+    {
+        ResponseResult LoginUser(LoginUserRequestModel model);
+
+        LoginUserResponseModel UserLogin(LoginUserRequestModel model);
+
+        Task<string> RegisterUser(RegisterUserRequestModel registerUserRequest);
+        UserResponseModel LoginUserDetailById(int id);
+        Task<bool> LoginUserProfileUpdate(AddUserDetailsRequestModel requestModel);
+    }
+}
